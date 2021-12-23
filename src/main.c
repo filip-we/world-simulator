85e7d9d47195dc5f631dad5c2ab502cd6d5bdad8
filip-we <filip.wettlen@gmail.com>
2021-12-23 22:28:03 +0100

    Added print matrix

--- src/main.c
+++ src/main.c
@@ -13,41 +13,95 @@
  */
 
 #include <stdio.h>
-#include <gsl/gsl_sf_bessel.h>
+#include <gsl/gsl_blas.h>
 
-#define WORLD_WIDTH 128
-#define WORLD_HEIGHT 32
+#define WORLD_WIDTH 32
+#define WORLD_HEIGHT 16
+const int startTemperature = 20;
+const int magmaTemperature = 99;
 
-char materials[WORLD_WIDTH][WORLD_HEIGHT];
-float temperatures[WORLD_WIDTH][WORLD_HEIGHT];
+char materials[WORLD_HEIGHT][WORLD_WIDTH];
+gsl_matrix *Temperature;
 
 const char MATERIAL_AIR = 'a';
 const char MATERIAL_MAGMA = 'm';
+
+static void init(){
+    Temperature = gsl_matrix_alloc(WORLD_HEIGHT, WORLD_WIDTH);
+    for(size_t i = 0; i<Temperature->size1; i++){
+        for(size_t j = 0; j<Temperature->size2; j++){
+            gsl_matrix_set(Temperature, i, j, startTemperature);
+        }
+    }
+    //for(size_t j = 0; j<Temperature->size2; j++){
+    //    gsl_matrix_set(Temperature, WORLD_HEIGHT-1, j, magmaTemperature);
+    //}
+}
+
+void printMatrix(gsl_matrix *m){
+    for(size_t i = m->size1-1; i>0; i--){
+        for(size_t j = 0; j<m->size2; j++){
+            printf("%3g  ", gsl_matrix_get(m, i, j));
+        }
+        printf("\n");
+    }
+}
+
+static void updateTemperature(){
+    gsl_matrix temp;
+    //gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, Temperature, A2, 0.0, temp);
+
+}
 
 static void printArray(){
     for(int h=WORLD_HEIGHT - 1; h>=0; h--) {
         printf("\n");
         for(int w=0; w<WORLD_WIDTH; w++) {
-            printf("%c", materials[w][h]);
+            printf("%c", materials[h][w]);
         }
     }
-}
-
-static void updateTemperature(){
 }
 
 int main(int argc, char *argv[]){
+    init();
+
     for(int h=0; h<WORLD_HEIGHT; h++) {
         for(int w=0; w<WORLD_WIDTH; w++) {
             if(h<5) {
-                materials[w][h] = MATERIAL_MAGMA;
+                materials[h][w] = MATERIAL_MAGMA;
             }
             else {
-                materials[w][h] = MATERIAL_AIR;
+                materials[h][w] = MATERIAL_AIR;
             }
         }
     }
+    printf("Printing matrix\n");
+    printMatrix(Temperature);
+}
+
+void test(){
     printf("\n");
     printArray();
+
+    gsl_matrix *A1;
+    gsl_matrix *A2;
+    gsl_matrix *R;
+    A1 = gsl_matrix_alloc(2, 2);
+    A2 = gsl_matrix_alloc(2, 2);
+    R = gsl_matrix_alloc(2, 2);
+
+    gsl_matrix_set(A1, 0, 0, 1);
+    gsl_matrix_set(A1, 0, 1, 2);
+    gsl_matrix_set(A1, 1, 0, 3);
+    gsl_matrix_set(A1, 1, 1, 4);
+
+    gsl_matrix_set(A2, 0, 0, 1);
+    gsl_matrix_set(A2, 0, 1, 2);
+    gsl_matrix_set(A2, 1, 0, 3);
+    gsl_matrix_set(A2, 1, 1, 4);
+
+    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, A1, A2, 0.0, R);
+    printf("\n");
+    //printMatrix(R);
 }
 
